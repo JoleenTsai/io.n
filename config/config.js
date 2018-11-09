4e960413ab826e832ed1ee625ad3d927e151ef1e
Garrett Fermo <garrettcfermo@gmail.com>
2018-11-08 19:12:05 -0800

    Folder Structure / Server Setup

---
+++ config/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": "rootroot",
+    "database": "surfboard_db",
+    "host": "127.0.0.1",
+    "port": 3306,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": "rootroot",
+    "database": "surfboard_db",
+    "host": "127.0.0.1",
+    "port": 3306,
+    "dialect": "mysql"
+  },
+  "production": {
+    "dialect": "mysql",
+    "use_env_variable": "JAWSDB_URL"
+  }
+}
