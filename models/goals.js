d8feb16883e440ac86d3ced9c06f9fc218da5ac0
Garrett Fermo <garrettcfermo@gmail.com>
2018-11-12 12:40:47 -0800

    Goals Model Completed

--- models/goals.js
+++ models/goals.js
@@ -1,6 +1,8 @@
 module.exports = (sequelize, DataTypes) => {
   return sequelize.define('Goals', {
-
-
+    id: DataTypes.INTEGER,
+    Date: DataTypes.DATEONLY ,
+    day_average_qty_goal: DataTypes.INTEGER,
+    day_average_sales_goal: DataTypes.FLOAT
   })
 }
