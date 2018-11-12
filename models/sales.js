2d9c535536e22fec3c579406548356dea76b07ba
Garrett Fermo <garrettcfermo@gmail.com>
2018-11-12 15:33:05 -0800

    Sales Model Completed

--- models/sales.js
+++ models/sales.js
@@ -1,6 +1,20 @@
 module.exports = (sequelize, DataTypes) => {
   return sequelize.define('Sales', {
-
-
+    sales_id: DataTypes.INTEGER,
+    employee_name: DataTypes.STRING,
+    employee_title: DataTypes.STRING,
+    employee_id: DataTypes.INTEGER,
+    transaction_date: DataTypes.DATEONLY,
+    transaction_time: DataTypes.TIME,
+    transaction_month: DataTypes.DATEONLY,
+    beer_name: DataTypes.STRING,
+    beer_id: DataTypes.INTEGER,
+    beer_type: DataTypes.STRING,
+    abv: DataTypes.FLOAT,
+    product_type: DataTypes.STRING,
+    total_oz: DataTypes.FLOAT,
+    cpu: DataTypes.FLOAT,
+    profit: DataTypes.FLOAT,
+    cost: DataTypes.FLOAT
   })
 }
