9b1421e2f30c86e077f89aed7f9069e486087b9c
Garrett Fermo <garrettcfermo@gmail.com>
2018-11-12 11:54:19 -0800

    Users Model Completed

--- models/user.js
+++ models/user.js
@@ -1,6 +1,10 @@
 module.exports = (sequelize, DataTypes) => {
   return sequelize.define('Users', {
-    
-    
+    id: DataTypes.INTEGER,
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    user_name: DataTypes.STRING,
+    password: DataTypes.STRING,
+    email: DataTypes.STRING
   })
 }
