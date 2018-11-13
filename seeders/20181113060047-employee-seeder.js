3b3807ebd6718918e24d9ba83339867d369f3466
Garrett Fermo <garrettcfermo@gmail.com>
2018-11-12 22:15:02 -0800

    Created the Employee Seed File

--- seeders/20181113060047-employee-seeder.js
+++ seeders/20181113060047-employee-seeder.js
@@ -12,16 +12,51 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert('Employees', [{
+      employee_id: 1,
+      first_name: "Garrett",
+      last_name: "Fermo",
+      full_name: "Garrett Fermo",
+      title: "Bartender",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      employee_id: 2,
+      first_name: "Adam",
+      last_name: "Openbrier",
+      full_name: "Adam Openbrier",
+      title: "Manager",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      employee_id: 3,
+      first_name: "Jean",
+      last_name: "Chung",
+      full_name: "Jean Chung",
+      title: "Supervisor",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      employee_id: 4,
+      first_name: "Joleen",
+      last_name: "Tsai",
+      full_name: "Joleen Tsai",
+      title: "Bartender",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      employee_id: 5,
+      first_name: "Radley",
+      last_name: "Eakle",
+      full_name: "Radley Eakle",
+      title: "Bartender",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }])
   },
 
   down: (queryInterface, Sequelize) => {
