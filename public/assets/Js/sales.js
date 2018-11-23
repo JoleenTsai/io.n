d075dbb93a4e2002fee3f2d581ae6003abfa991b
Garrett Fermo <garrettcfermo@gmail.com>
2018-11-23 15:35:32 -0800

    Sales Goals Graph Completed

--- public/assets/Js/sales.js
+++ public/assets/Js/sales.js
@@ -15,7 +15,7 @@ function breweryData() {
       hourSalesGraph(r)
       topItemsGraph(r)
       employeeSalesGraph(r)
-      salesGoalGraph()
+      salesGoalGraph(r)
     })
     .catch(e => console.log(e))
 }
@@ -364,9 +364,9 @@ function employeeSalesGraph(r) {
         display: false
       },
       scales: {
-        xAxes:[{
-          ticks:{
-            autoSkip:false
+        xAxes: [{
+          ticks: {
+            autoSkip: false
           }
         }],
         yAxes: [{
@@ -384,46 +384,75 @@ function employeeSalesGraph(r) {
 }
 
 // Sales Goals Graph
+function salesGoalGraph(r) {
 
-function salesGoalGraph() {
-  let sGoals = document.getElementById("salesGoals");
-  let salesGoals = new Chart(sGoals, {
-    type: 'bar',
-    data: {
-      labels: ["Achievement 1", "Achievement 2", "Achievement 3", "Achievement 4", "Achievement 5"],
-      datasets: [{
-        label: 'Sales Goals',
-        data: [985, 728, 507, 467, 456],
-        backgroundColor: [
-          'rgb(63, 66, 52)',
-          'rgb(63, 66, 52)',
-          'rgb(63, 66, 52)',
-          'rgb(63, 66, 52)',
-          'rgb(63, 66, 52)',
-        ]
-      }]
-    },
-    options: {
-      legend: {
-        display: false
-      },
-      scales: {
-        xAxes: [{
-          ticks: {
-            autoSkip: false
-          }
-        }],
-        yAxes: [{
-          ticks: {
-            beginAtZero: true,
-            callback: function (value, index, values) {
-              return '$' + value;
-            }
-          }
-        }]
-      }
-    }
+  // Finding the Total Sales
+  let totalSales = 0
+  r.forEach(item => {
+    totalSales += item.cost
   })
+
+  fetch("/goals")
+  .then(d => d.json())
+  .then(d => {
+    console.log(d)
+
+    let totalGoalSales = 0
+  
+    // Start Date to Pull From
+    let y = moment(startDate, "YYYY-MM-DD").startOf("month").format("YYYY-MM-DD")
+    console.log(y)
+
+    // How many days between the start Date and End Date
+    let dayDiff = 1 + moment(endDate, "YYYY-MM-DD").diff(moment(startDate, "YYYY-MM-DD"), "days")
+    console.log(dayDiff)
+    
+    d.forEach(goal => {
+        if (y === goal.Date) {
+          totalGoalSales = goal.day_average_sales_goal * dayDiff
+          console.log(totalGoalSales)
+          console.log(totalSales)
+
+        let percentageGoal = Math.round(totalSales / totalGoalSales *100)/100
+        console.log(percentageGoal)
+
+        let diffpercentageGoal = 0
+
+        if(percentageGoal<=1){
+          diffpercentageGoal = Math.round((1- percentageGoal)*100)/100
+        }
+
+        console.log(diffpercentageGoal) 
+
+          let sGoals = document.getElementById("salesGoals");
+          let salesGoals = new Chart(sGoals, {
+            type: 'doughnut',
+            data: {
+              labels: ["Goal Acheived", "Goal Left"],
+              datasets: [{
+                label: 'Sales by Employee',
+                data: [percentageGoal, diffpercentageGoal],
+                backgroundColor: [
+                  'rgb(209, 168, 39)',
+                  'rgb(161, 187, 208)'
+                ]
+              }]
+            },
+            options: {
+              showAllTooltips: true
+            }
+          })
+
+
+
+
+        }
+      })
+    })
+    .catch(e => console.log(e))
+    
+
+  
 }
 
 breweryData()
