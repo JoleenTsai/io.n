f6dbe5c1dacfa5aceeaecba6053d043a4573c666
aOpenbrier <aopenbrier@gmail.com>
2019-02-08 22:34:33 -0800

    fix  c.r.U.D. routes

--- routes/salesRoutes.js
+++ routes/salesRoutes.js
@@ -35,69 +35,22 @@ module.exports = app => {
     })
 
     // Update sales transaction retroactively
-    app.put('/sales/:type-:fromdate-:todate', (req, res) => {
-        let fromDate = moment(req.params.fromdate).format('YYYY-MM-DD')
-        let toDate = moment(req.params.todate).format('YYYY-MM-DD')
-        switch (req.params.type) {
-            case 'move':
-                // Move whole month
-                const days = moment(req.params.todate).endOf('month').format('DD')
+    app.put('/sales/:id', (req, res) => {
                 res.sendStatus(200)
-                for (let i = 1; i <= days; i++) {
-                    fromDate = moment(fromDate).date(i).format('YYYY-MM-DD')
-                    toDate = moment(toDate).date(i).format('YYYY-MM-DD')
-                    db.Sales.update(
-                        {
-                            transaction_date: toDate
-                        },
+                    db.Sales.update(req.body,
                         {
                             where: {
-                                transaction_date: fromDate
-                            },
-                            fields: ['transaction_date']
+                                id: req.params.id
+                            }
                         })
                         .then(r => console.log(r)).catch(e => console.log(e))
-                }
-                break
-            case 'copy':
-                // Copy single day
-                res.sendStatus(200) 
-                db.Sales.findAll({
-                    where: {
-                        transaction_date: fromDate
-                    }
-                })
-                    .then(transactions => {
-                        let transArr = []
-                        transactions.forEach(trans => {
-                            transArr.push({
-                                    employee_id: trans.dataValues.employee_id,
-                                    transaction_date: toDate,
-                                    transaction_time: trans.dataValues.transaction_time,
-                                    beer_id: trans.dataValues.beer_id,
-                                    product_type: trans.dataValues.product_type,
-                                    total_oz: trans.dataValues.total_oz,
-                                    cost: trans.dataValues.cost
-                            })
-                        })
-                        db.Sales.bulkCreate(transArr)
-                        .then(r => console.log(r))
-                        .catch(e=> console.log(e))
-                    })
-                    .catch(e => console.log(e))
-
-                break
-            default:
-                // do nothing
-                break
-        }
     })
 
     //Delete sale
-    app.delete("/sales/:date", function (req, res) {
-        db.Sales.destroy({
+    app.delete("/sales/:id", function (req, res) {
+        db.Products.destroy({
             where: {
-                transaction_date: req.params.date
+                id: req.params.id
             }
         })
             .then(r => res.sendStatus(200))
